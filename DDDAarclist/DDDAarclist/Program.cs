98db7dc7c536f7b0c1506bdd24bb58edaff7dee0
Vuze <vuze.mhf2resg@gmail.com>
2016-01-16 20:49:49 +0100

    Added DDDAarclist tool, updated readme

---
+++ DDDAarclist/DDDAarclist/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDAarclist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print header
+            Console.WriteLine("DDDAarclist by MHVuze\n");
+
+            // Check arguments
+            if (args.Length < 1)
+            {
+                Console.WriteLine("ERROR: Invalid amount of arguments.\n");
+                Console.WriteLine("Use DDDAarclist <input_folder> [flag]");
+                Console.WriteLine("Available flag: -f for full path name");
+                return;
+            }
+
+            // Assign arguments
+            string folder = args[0];
+            string flag = "";
+            if (args.Length > 1)
+                flag = args[1];
+
+            // Check for folder / file
+            if (!Directory.Exists(folder))
+            {
+                Console.WriteLine("ERROR: Input folder does not exist.");
+                return;
+            }
+
+            // Setup csv writer
+            StreamWriter csv = new StreamWriter("ArcInfo.csv", false);
+            csv.WriteLine("Archive,File,Type,CSize,FSize,Constant,Offset");
+            string file_info;
+
+            // Recursively work through archives
+            foreach (string archive in Directory.EnumerateFiles(folder, "*.arc", SearchOption.AllDirectories))
+            {
+                // Read file to memory
+                MemoryStream ms_input = new MemoryStream(File.ReadAllBytes(archive));
+                BinaryReader br_input = new BinaryReader(ms_input);
+
+                // Check file magic
+                int magic = br_input.ReadInt32();
+                int version = br_input.ReadInt16();
+                if (magic != 0x00435241 || version != 0x07)
+                {
+                    Console.WriteLine("ERROR: File is not a DD:DA (PC) .arc file.");
+                    return;
+                }
+
+                // Process remaining header
+                int count = br_input.ReadInt16();
+
+                // Process entries
+                for (int i = 0; i < count; i++)
+                {
+                    // Process entry block
+                    byte[] name = br_input.ReadBytes(0x40).Where(b => b != 0x00).ToArray();
+                    int extension = br_input.ReadInt32();
+                    int comp_size = br_input.ReadInt32();
+                    byte[] full_size_array = br_input.ReadBytes(3);
+                    byte constant = br_input.ReadByte();
+                    int offset = br_input.ReadInt32();
+
+                    // Process entry info
+                    string string_name = System.Text.Encoding.UTF8.GetString(name);
+                    ExtensionHandler.IntToString(extension);
+
+                    byte[] full_size_array2 = new byte[4];
+                    full_size_array.CopyTo(full_size_array2, 0);
+                    int full_size = BitConverter.ToInt32(full_size_array2, 0);
+
+                    // Write info to csv
+                    string path = archive.Replace((folder + "\\"), "");
+
+                    if (flag == "-full")
+                        path = archive;
+
+                    file_info =
+                        path + "," +
+                        string_name + "." + ExtensionHandler.string_extension + "," +
+                        ExtensionHandler.string_extension + "," +
+                        comp_size + "," +
+                        full_size + "," +
+                        constant + "," +
+                        offset;
+                    csv.WriteLine(file_info);
+
+                    // Seek to next entry info block
+                    br_input.BaseStream.Seek(0x08 + (i + 1) * 0x50, SeekOrigin.Begin);
+                }
+                Console.WriteLine("INFO: Successfully processed " + archive);
+            }
+            csv.Close();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -16,6 +16,16 @@ DDDAarc -p input_folder output.arc
 
 I have also included drag'n'drop batch files for unpacking and repacking. Just drag input.arc / input_folder on the matching batch file.
 
+##DDDAarclist
+Processes all .arc archive files from a given folder recursively. Gathered info is printed in comma-seperated format, no files are extracted.
+
+Useful for finding duplicates or files of interest. You can force full path printing for archive names by using the flag `-full`.
+
+Syntax:
+```
+DDDAarclist input_folder [flag]
+```
+
 ##DDDApck
 Unpacks .pck package files. Repacking is not planned. Only tested on message.pck.
 
@@ -29,3 +39,15 @@ DDDApck input.pck
 ##Requirements
 * Microsoft .NET Framework 4.5
 * [Zlib.net (supplied)](http://www.componentace.com/zlib_.NET.htm) (for building only)
+
+###License info Zlib.net
+Copyright (c) 2006-2007, ComponentAce
+http://www.componentace.com
+All rights reserved.
+
+<sub>Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+Neither the name of ComponentAce nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.</sub>
